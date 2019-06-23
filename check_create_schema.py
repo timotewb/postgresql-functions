b2ad533ba0b614ff8136456f3a96237d49e811d7
timotewb <45491056+timotewb@users.noreply.github.com>
2019-06-23 11:06:11 +0100

    Create check_create_schema.py

---
+++ check_create_schema.py
@@ -0,0 +1,17 @@
+def check_create_schema(schema, connection):
+
+  #--- Check and Create Schema
+  # schema: string, name of schema to create or load into
+  # connection: string, contining connection details e.g. host=<ip address> dbname=<db name> user=<user name> password=<pass word>
+
+  conn = pgclass.psycopg2.connect(connection)
+  cursor = conn.cursor()
+  cursor.execute("select distinct schema_name from information_schema.schemata where schema_name = '"+schema+"'")
+  if cursor.rowcount >= 1:
+    pass
+  else:
+    cursor.execute('create schema '+schema+' authorization "superusers"')
+    conn.commit()
+    cursor.execute('GRANT ALL ON SCHEMA '+schema+' TO "superusers"')
+    conn.commit()
+  conn.close()
