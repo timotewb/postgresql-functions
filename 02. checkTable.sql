413e2c6ed545ce535504b43bbb11479fed0a0d6d
timotewb <45491056+timotewb@users.noreply.github.com>
2019-07-12 08:01:12 +0100

    Update and rename 02. checkTable to 02. checkTable.sql

---
+++ 02. checkTable.sql
@@ -0,0 +1,16 @@
+def checkTable(schema,table,connection):
+
+    #--- Check Table
+    # schema: string, name of schema to create or load into
+    # table: string, name of table to create or load into
+    # connection: string, contining connection details e.g. host=<ip address> dbname=<db name> user=<user name> password=<pass word>
+
+    conn = psycopg2.connect(connection)
+    cursor = conn.cursor()
+    cursor.execute("select distinct table_name from information_schema.tables where upper(table_schema) = upper('"+schema+"') and upper(table_name) = upper('"+table+"')")
+    if cursor.rowcount >= 1:
+        return True
+    else:
+        return False
+        
+    conn.close()
